7a7a2904a719d5dd1aa10eef4f3914eaefd54654
Clay Zhu <clayzhu@163.com>
2017-03-19 22:24:58 +0800

    创建项目。

---
+++ CZBluetoothDemo/CZBluetoothDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CZBluetoothDemo
+//
+//  Created by ug19 on 2017/3/19.
+//  Copyright © 2017年 Ugood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CZBluetoothDemo/CZBluetoothDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CZBluetoothDemo
+//
+//  Created by ug19 on 2017/3/19.
+//  Copyright © 2017年 Ugood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
